91bf1abeae9b1d07fe543b007cadde858d03992d
Marcos <marcosv.java@gmail.com>
2026-06-09 16:36:16 -0400

    NEW: Session class and SessionStatus enum

--- src/main/java/edu/marcos/saxtracker/model/Session.java
+++ src/main/java/edu/marcos/saxtracker/model/Session.java
@@ -1,4 +1,22 @@
 package edu.marcos.saxtracker.model;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "sessao")
 public class Session {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false, name = "data")
+    private LocalDate date;
+    @Column(name = "observacao")
+    private String notes;
+    @Column(name = "ativo", nullable = false)
+    private SessionStatus status;
+    @ManyToOne
+    @JoinColumn(name =  "rotina_id",nullable = false)
+    private Routine routine;
 }
