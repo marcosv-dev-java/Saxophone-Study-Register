a285307b16fe6bdd0cb589f8fcb788b57e283166
Marcos <marcosv.java@gmail.com>
2026-06-11 16:02:03 -0400

    feat: method findByType in exercise repository

--- src/main/java/edu/marcos/saxtracker/repository/ExerciseRepository.java
+++ src/main/java/edu/marcos/saxtracker/repository/ExerciseRepository.java
@@ -1,7 +1,11 @@
 package edu.marcos.saxtracker.repository;
 
 import edu.marcos.saxtracker.model.Exercise;
+import edu.marcos.saxtracker.model.ExerciseType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
+    public List<Exercise> findByType(ExerciseType type);
 }
