3787cab1802ccfb4b4ca322c90dc189d7ff0b68b
Marcos <marcosv.java@gmail.com>
2026-06-18 15:03:29 -0400

    feat: method create routine working and refactores list exercises to summary method

--- src/main/java/edu/marcos/saxtracker/service/RoutineService.java
+++ src/main/java/edu/marcos/saxtracker/service/RoutineService.java
@@ -25,22 +25,19 @@ public class RoutineService {
 
     private Routine requestToEntity(RoutineRequest request) {
         List<Exercise> exercises = exerciseRepository.findAllById(request.exerciseIds());
-        return new Routine(request.name(), exercises,request.description());
-    }
-    private List<ExerciseSummary> listIdExercisesToSummary(List<Long> exercisesId){
-        List<ExerciseSummary> exercisesSummary = new ArrayList<>();
-        List<Exercise> exercises = exerciseRepository.findAllById(exercisesId);
-        for (Exercise exercise : exercises) {
-            exercisesSummary.add(new ExerciseSummary(exercise.getId(), exercise.getName()));
-        }
-        if(exercisesId.size() != exercises.size()) {
+        if (exercises.size() != request.exerciseIds().size()) {
             List<Long> foundIds = exercises.stream().map(Exercise::getId).toList();
-            List<Long> missingIds = exercisesId.stream()
+            List<Long> missingIds = request.exerciseIds().stream()
                     .filter(id -> !foundIds.contains(id))
                     .toList();
             throw new ResourceNotFoundException("Exercises not found: " + missingIds);
         }
-        return exercisesSummary;
+        return new Routine(request.name(), exercises,request.description());
+    }
+    private List<ExerciseSummary> listExercisesToSummary(List<Exercise> exercises){
+        return exercises.stream()
+                .map(e -> new ExerciseSummary(e.getId(), e.getName()))
+                .toList();
     }
 
     public RoutineResponse createRoutine(RoutineRequest request){
@@ -48,7 +45,7 @@ public class RoutineService {
         repository.save(routine);
         return new RoutineResponse(routine.getId(),routine.getName(),routine.getDescription(),
                 routine.getActive(),
-                this.listIdExercisesToSummary(request.exerciseIds()));
+                this.listExercisesToSummary(routine.getExercises()));
 
     }
 
