c8fb57dbe66fafa2b44216b51e9396f7e2ade75d
Marcos <marcosv.java@gmail.com>
2026-06-18 16:30:04 -0400

    feat: finished routine service methods

--- src/main/java/edu/marcos/saxtracker/repository/RoutineRepository.java
+++ src/main/java/edu/marcos/saxtracker/repository/RoutineRepository.java
@@ -3,10 +3,16 @@ package edu.marcos.saxtracker.repository;
 import edu.marcos.saxtracker.model.Routine;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RoutineRepository extends JpaRepository<Routine, Long> {
     @Query("SELECT DISTINCT r FROM Routine r JOIN FETCH r.exercises")
     List<Routine> findAllWithExercises();
+
+
+    @Query("SELECT DISTINCT r FROM Routine r JOIN FETCH r.exercises WHERE r.id = :id")
+    Optional<Routine> findByIdWithExercises(@Param("id") Long id);
 }
--- src/main/java/edu/marcos/saxtracker/service/RoutineService.java
+++ src/main/java/edu/marcos/saxtracker/service/RoutineService.java
@@ -23,16 +23,20 @@ public class RoutineService {
         this.repository = repository;
         this.exerciseRepository = exerciseRepository;
     }
-
-    private Routine requestToEntity(RoutineRequest request) {
-        List<Exercise> exercises = exerciseRepository.findAllById(request.exerciseIds());
-        if (exercises.size() != request.exerciseIds().size()) {
+    private List<Exercise> idListToExerciseList(List<Long> ids) {
+        List<Exercise> exercises = exerciseRepository.findAllById(ids);
+        if (exercises.size() != ids.size()) {
             List<Long> foundIds = exercises.stream().map(Exercise::getId).toList();
-            List<Long> missingIds = request.exerciseIds().stream()
+            List<Long> missingIds = ids.stream()
                     .filter(id -> !foundIds.contains(id))
                     .toList();
             throw new ResourceNotFoundException("Exercises not found: " + missingIds);
         }
+        return exercises;
+    }
+
+    private Routine requestToEntity(RoutineRequest request) {
+        List<Exercise> exercises = idListToExerciseList(request.exerciseIds());
         return new Routine(request.name(), exercises,request.description());
     }
     private List<ExerciseSummary> listExercisesToSummary(List<Exercise> exercises){
@@ -42,7 +46,7 @@ public class RoutineService {
     }
 
     private Routine findRoutineById(Long id){
-        return repository.findById(id)
+        return repository.findByIdWithExercises(id)
                 .orElseThrow(()->new ResourceNotFoundException("Routine not found with id: " + id));
     }
     private RoutineResponse entityToResponse(Routine routine){
@@ -72,12 +76,22 @@ public class RoutineService {
 
     public RoutineResponse updateRoutine(Long id,RoutineUpdateRequest updateRequest){
         Routine routine = findRoutineById(id);
-        if(!routine.getActive()) throw new IllegalStateException("Cannot update an inactive exercise");
+        if(!routine.getActive()) throw new IllegalStateException("Cannot update an inactive routine");
         if(updateRequest.name() != null) routine.setName(updateRequest.name());
         if(updateRequest.description() != null) routine.setDescription(updateRequest.description());
-        if(updateRequest.description() != null) routine.setDescription(updateRequest.description());
+        if(updateRequest.exerciseIds() != null && !updateRequest.exerciseIds().isEmpty())
+            routine.setExercises(idListToExerciseList(updateRequest.exerciseIds()));
+        repository.save(routine);
         return entityToResponse(routine);
+    }
 
+    public RoutineResponse deactivateRoutine(Long id){
+        Routine routine = findRoutineById(id);
+        if(!routine.getActive())
+            throw new IllegalStateException("The active status is already false");
+        routine.setActive(false);
+        repository.save(routine);
+        return entityToResponse(routine);
     }
 
 }
