20e1a99826fbe232b035cc397ce581f49e10c9a7
Marcos <marcosv.java@gmail.com>
2026-06-19 14:10:51 -0400

    feat: method post in routine controller

--- src/main/java/edu/marcos/saxtracker/controller/RoutineController.java
+++ src/main/java/edu/marcos/saxtracker/controller/RoutineController.java
@@ -1,8 +1,17 @@
 package edu.marcos.saxtracker.controller;
 
+import edu.marcos.saxtracker.dto.routine.RoutineRequest;
+import edu.marcos.saxtracker.dto.routine.RoutineResponse;
 import edu.marcos.saxtracker.service.RoutineService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/rotinas")
@@ -12,6 +21,16 @@ public class RoutineController {
 
     public RoutineController(RoutineService service) {
         this.service = service;
+    }
+    @PostMapping
+    public ResponseEntity<RoutineResponse> addRoutine(@RequestBody @Valid RoutineRequest request){
+        RoutineResponse response = service.createRoutine(request);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(response.id())
+                .toUri();
+        return ResponseEntity.created(uri).body(response);
     }
 
 }
