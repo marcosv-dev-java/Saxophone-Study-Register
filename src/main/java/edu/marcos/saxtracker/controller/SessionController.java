3c54ce1531a2f5b0e5fc19c5679e8357830b05d3
Marcos <marcosv.java@gmail.com>
2026-06-20 09:43:40 -0400

    feat: new Session controller with basic http methods get/post

---
+++ src/main/java/edu/marcos/saxtracker/controller/SessionController.java
@@ -0,0 +1,49 @@
+package edu.marcos.saxtracker.controller;
+
+
+import edu.marcos.saxtracker.dto.session.SessionRequest;
+import edu.marcos.saxtracker.dto.session.SessionResponse;
+import edu.marcos.saxtracker.service.SessionService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("/sessoes")
+public class SessionController {
+    private final SessionService service;
+    public SessionController(SessionService service){
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<SessionResponse> createSession(@RequestBody @Valid SessionRequest request){
+        SessionResponse response = service.createSession(request);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(response.id())
+                .toUri();
+        return ResponseEntity.created(uri).body(response);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<SessionResponse> findSessionById(@PathVariable Long id){
+        return ResponseEntity.ok().body(service.findById(id));
+    }
+    @GetMapping("/data/{date}")
+    public ResponseEntity<SessionResponse> findByDate(
+            @PathVariable LocalDate date
+    ) {
+        return ResponseEntity.ok(service.findByDate(date));
+    }
+    @GetMapping
+    public ResponseEntity<List<SessionResponse>> findAll(){
+        return  ResponseEntity.ok().body(service.findAll());
+    }
+
+}
