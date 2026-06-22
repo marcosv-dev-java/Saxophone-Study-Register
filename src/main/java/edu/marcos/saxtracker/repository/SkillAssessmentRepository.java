e092e0fa4b1edc988e71ccbfe3ff692dc9792069
Marcos <marcosv.java@gmail.com>
2026-06-22 15:58:21 -0400

    feat: new method in repository to verify if the skill has already been evaluated

--- src/main/java/edu/marcos/saxtracker/repository/SkillAssessmentRepository.java
+++ src/main/java/edu/marcos/saxtracker/repository/SkillAssessmentRepository.java
@@ -1,7 +1,10 @@
 package edu.marcos.saxtracker.repository;
 
+import edu.marcos.saxtracker.model.Skill;
 import edu.marcos.saxtracker.model.SkillAssessment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SkillAssessmentRepository extends JpaRepository<SkillAssessment, Long>{
+    boolean existsBySkillAndSession_Id(Skill skill, Long sessionId);
+    // verifica se já avaliou essa habilidade na sessão
 }
