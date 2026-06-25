784f6ac9e4c8dd98a705fa56a2b3b802499b8e90
Marcos <marcosv.java@gmail.com>
2026-06-25 19:29:52 -0400

    test: add error scenario tests for SkillAssessmentService

--- src/test/java/edu/marcos/saxtracker/service/SkillAssessmentServiceTest.java
+++ src/test/java/edu/marcos/saxtracker/service/SkillAssessmentServiceTest.java
@@ -4,6 +4,7 @@ import edu.marcos.saxtracker.dto.assessment.SkillAssessmentRequest;
 import edu.marcos.saxtracker.dto.assessment.SkillAssessmentResponse;
 import edu.marcos.saxtracker.dto.assessment.SkillAssessmentResponseTest;
 import edu.marcos.saxtracker.dto.session.SessionSummary;
+import edu.marcos.saxtracker.exceptions.ResourceNotFoundException;
 import edu.marcos.saxtracker.model.Session;
 import edu.marcos.saxtracker.model.SessionStatus;
 import edu.marcos.saxtracker.model.Skill;
@@ -21,6 +22,7 @@ import java.time.LocalDate;
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -41,8 +43,8 @@ public class SkillAssessmentServiceTest {
     void setUp() {
         sessionId = 1L;
         sessionMock = Mockito.mock(Session.class);
-        when(sessionMock.getDate()).thenReturn(LocalDate.now());
-        when(sessionMock.getId()).thenReturn(sessionId);
+        lenient().when(sessionMock.getDate()).thenReturn(LocalDate.now());
+        lenient().when(sessionMock.getId()).thenReturn(sessionId);
     }
 
     @Test
@@ -79,5 +81,93 @@ public class SkillAssessmentServiceTest {
         verify(sessionService).checkAndCloseSessionIfComplete(sessionId);
 
     }
+    @Test
+    void shouldThrowExceptionWhenSessionNotFound() {
+        when(sessionRepository.findById(sessionId)).thenReturn(Optional.empty());
+        List<SkillAssessmentRequest> requests = List.of(
+                new SkillAssessmentRequest(Skill.ARTICULATION, 1),
+                new SkillAssessmentRequest(Skill.BREATHING, 2),
+                new SkillAssessmentRequest(Skill.READING, 3),
+                new SkillAssessmentRequest(Skill.TIMBRE, 4),
+                new SkillAssessmentRequest(Skill.TUNING, 5)
+        );
+        ResourceNotFoundException exception = assertThrows(ResourceNotFoundException.class, () -> service.evaluateSession(sessionId,requests));
+        assertEquals("Session not found with id " + sessionId,exception.getMessage());
+    }
+    @Test
+    void shouldThrowExceptionWhenSessionIsClosed() {
+        when(sessionRepository.findById(sessionId)).thenReturn(Optional.of(sessionMock));
+        when(sessionMock.getStatus()).thenReturn(SessionStatus.CLOSED);
+        List<SkillAssessmentRequest> requests = List.of(
+                new SkillAssessmentRequest(Skill.ARTICULATION, 1),
+                new SkillAssessmentRequest(Skill.BREATHING, 2),
+                new SkillAssessmentRequest(Skill.READING, 3),
+                new SkillAssessmentRequest(Skill.TIMBRE, 4),
+                new SkillAssessmentRequest(Skill.TUNING, 5)
+        );
+        IllegalStateException exception = assertThrows(IllegalStateException.class, () -> service.evaluateSession(sessionId,requests));
+        assertEquals("Session is already closed",exception.getMessage());
+    }
+    @Test
+    void shouldThrowExceptionWhenValueIsInvalid() {
+        when(sessionRepository.findById(sessionId)).thenReturn(Optional.of(sessionMock));
+        when(sessionMock.getStatus()).thenReturn(SessionStatus.OPEN);
+        List<SkillAssessmentRequest> requests = List.of(
+                new SkillAssessmentRequest(Skill.ARTICULATION, 1),
+                new SkillAssessmentRequest(Skill.BREATHING, 2),
+                new SkillAssessmentRequest(Skill.READING, 11),
+                new SkillAssessmentRequest(Skill.TIMBRE, 4),
+                new SkillAssessmentRequest(Skill.TUNING, 0)
+        );
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> service.evaluateSession(sessionId,requests));
+        assertEquals("Value must be between 1 and 10",exception.getMessage());
+    }
+    @Test
+    void shouldThrowExceptionWhenAssessmentAlreadyExistsInSession() {
+        when(sessionRepository.findById(sessionId)).thenReturn(Optional.of(sessionMock));
+        when(sessionMock.getStatus()).thenReturn(SessionStatus.OPEN);
+        when(repository.existsBySkillAndSession_Id(any(),any())).thenReturn(true);
+        List<SkillAssessmentRequest> requests = List.of(
+                new SkillAssessmentRequest(Skill.ARTICULATION, 1),
+                new SkillAssessmentRequest(Skill.BREATHING, 2),
+                new SkillAssessmentRequest(Skill.READING, 3),
+                new SkillAssessmentRequest(Skill.TIMBRE, 4),
+                new SkillAssessmentRequest(Skill.TUNING, 5)
+        );
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> service.evaluateSession(sessionId,requests));
+        assertEquals("Skill assessment to "+ requests.getFirst().skill().toString() +" already exists in this session",exception.getMessage());
+    }
+    @Test
+    void shouldThrowExceptionWhenAssessmentIsDuplicated(){
+        when(sessionRepository.findById(sessionId)).thenReturn(Optional.of(sessionMock));
+        when(sessionMock.getStatus()).thenReturn(SessionStatus.OPEN);
+        when(repository.existsBySkillAndSession_Id(any(),any())).thenReturn(false);
+        List<SkillAssessmentRequest> requests = List.of(
+                new SkillAssessmentRequest(Skill.ARTICULATION, 1),
+                new SkillAssessmentRequest(Skill.ARTICULATION, 5),
+                new SkillAssessmentRequest(Skill.BREATHING, 2),
+                new SkillAssessmentRequest(Skill.BREATHING, 4),
+                new SkillAssessmentRequest(Skill.READING, 3),
+                new SkillAssessmentRequest(Skill.TIMBRE, 4),
+                new SkillAssessmentRequest(Skill.TUNING, 5)
+        );
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> service.evaluateSession(sessionId,requests));
+        assertEquals("2 assessment is duplicated ",exception.getMessage());
+    }
+    @Test
+    void shouldThrowExceptionWhenIsMissingSkill(){
+        when(sessionRepository.findById(sessionId)).thenReturn(Optional.of(sessionMock));
+        when(sessionMock.getStatus()).thenReturn(SessionStatus.OPEN);
+        when(repository.existsBySkillAndSession_Id(any(),any())).thenReturn(false);
+        List<SkillAssessmentRequest> requests = List.of(
+                new SkillAssessmentRequest(Skill.ARTICULATION, 1),
+                new SkillAssessmentRequest(Skill.BREATHING, 2),
+                new SkillAssessmentRequest(Skill.READING, 3),
+                new SkillAssessmentRequest(Skill.TIMBRE, 4)
+        );
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> service.evaluateSession(sessionId,requests));
+        assertEquals("All skills must be assessed",exception.getMessage());
+    }
+
 
 }
