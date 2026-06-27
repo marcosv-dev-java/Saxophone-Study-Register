d5e8e5b38fe78901431f8b467973512ab43a0241
Marcos <marcosv.java@gmail.com>
2026-06-27 18:05:20 -0400

    feat: new skill progress response and new method private average filtered by skill in progress service

---
+++ src/main/java/edu/marcos/saxtracker/dto/progress/SkillProgressResponse.java
@@ -0,0 +1,11 @@
+package edu.marcos.saxtracker.dto.progress;
+
+import edu.marcos.saxtracker.model.Skill;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record SkillProgressResponse(
+        Skill skill,
+        @Schema(description = "Média das notas no período. Retorna null se não houver avaliações registradas nesse período.")
+        Double media
+) {
+}
--- src/main/java/edu/marcos/saxtracker/service/ProgressService.java
+++ src/main/java/edu/marcos/saxtracker/service/ProgressService.java
@@ -1,18 +1,23 @@
 package edu.marcos.saxtracker.service;
-
-
-import edu.marcos.saxtracker.repository.SessionRepository;
+import edu.marcos.saxtracker.dto.progress.SkillProgressResponse;
+import edu.marcos.saxtracker.model.Skill;
+import edu.marcos.saxtracker.repository.SkillAssessmentRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 
+
 @Service
 public class ProgressService {
-    private SessionRepository sessionRepository;
+    private SkillAssessmentRepository assessmentRepositoryRepository;
 
-
-    public ProgressService(SessionRepository sessionRepository) {
-        this.sessionRepository = sessionRepository;
+    public ProgressService(SkillAssessmentRepository assessmentRepositoryRepository) {
+        this.assessmentRepositoryRepository = assessmentRepositoryRepository;
     }
 
+    private SkillProgressResponse averageInPeriod_filterBySkill(Skill skill, LocalDate start, LocalDate end) {
+        return new SkillProgressResponse(skill, assessmentRepositoryRepository.findAverageBySkillAndPeriod(
+                skill, start, end
+        ));
+    }
 }
